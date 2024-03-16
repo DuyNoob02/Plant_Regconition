8e89dc24ef36de7e8955085ea852eb3af00aa944
DuyNoob02 <nguyenkhachuy25122002@gmail.com>
2024-03-16 21:13:17 +0700

    update

--- AppServer/Helper/connections_redis.js
+++ AppServer/Helper/connections_redis.js
@@ -2,7 +2,7 @@ const redis = require('redis');
 
 const client = redis.createClient({
     port: 6379,
-    host: "127.0.0.1"
+    host: "https://nodeserver-plant.onrender.com/"
 })
 
 client.ping((err, pong)=>{
