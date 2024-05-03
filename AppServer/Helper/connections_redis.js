b11ffa498b3fb6d8e136bf8f9cdddfee668981b3
DuyNoob02 <nguyenkhachuy25122002@gmail.com>
2024-05-03 20:12:02 +0700

    update

--- AppServer/Helper/connections_redis.js
+++ AppServer/Helper/connections_redis.js
@@ -1,13 +1,14 @@
 const redis = require('redis');
 
-const client = redis.createClient({
-    port: 6379,
-    host: "redis://red-co17rbcf7o1s73ca01u0"
-})
 // const client = redis.createClient({
 //     port: 6379,
-//     host: "127.0.0.1"
+//     host: "rediss://red-co17rbcf7o1s73ca01u0:pEgdv6Y5VeMqkpK9YfAKyZU9m0SjhfO0@singapore-redis.render.com"
 // })
+// rediss://red-co17rbcf7o1s73ca01u0:pEgdv6Y5VeMqkpK9YfAKyZU9m0SjhfO0@singapore-redis.render.com:6379
+const client = redis.createClient({
+    port: 6379,
+    host: "127.0.0.1"
+})
 
 client.ping((err, pong)=>{
     console.log(pong);
